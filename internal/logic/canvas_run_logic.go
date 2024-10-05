f85b96f3ea36f77b1ab35385a77e61221e7bfad0
xuetu.qiang <1227694865@qq.com>
2024-10-06 00:31:18 +0800

    feat rule run api

--- internal/logic/canvas_run_logic.go
+++ internal/logic/canvas_run_logic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"github.com/rulego/rulego/utils/json"
+	"github.com/zeromicro/x/errors"
+	"workflow/internal/rolego"
 
 	"workflow/internal/svc"
 	"workflow/internal/types"
@@ -25,9 +28,29 @@ func NewCanvasRunLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CanvasR
 
 func (l *CanvasRunLogic) CanvasRun(req *types.CanvasRunRequest) (resp *types.CanvasRunResponse, err error) {
 	// 1. 读取点、线
+	// 查询点
+	nodes, err := l.svcCtx.NodeModel.FindOneByWorkSpace(l.ctx, req.WorkSpaceId)
+	if err != nil {
+		return nil, errors.New(int(SystemOrmError), "查询节点失败")
+	}
 
+	// 查询边
+	edges, err := l.svcCtx.EdgeModel.FindOneByWorkSpace(l.ctx, req.WorkSpaceId)
+	if err != nil {
+		return nil, errors.New(int(SystemOrmError), "查询边失败")
+	}
+	l.Info(nodes)
+	l.Infov(edges)
 	// 2. 拼接 json
-
+	rule := rolego.Rule{
+		RuleChain: rolego.RuleChain{},
+		Metadata:  rolego.Metadata{},
+	}
+	ruleJson, err := json.Marshal(rule)
+	if err != nil {
+		return nil, errors.New(int(SystemOrmError), "序列化规则失败")
+	}
+	l.Info(ruleJson)
 	// 3. 加载到链池 记录 md5 新建 or 重新加载
 
 	// 4. doMsg
---
+++ internal/rolego/role_model.go
@@ -0,0 +1,29 @@
+package rolego
+
+type Rule struct {
+	RuleChain RuleChain `json:"ruleChain"`
+	Metadata  Metadata  `json:"metadata"`
+}
+
+type RuleChain struct {
+	Id string `json:"id"`
+}
+
+type Metadata struct {
+	// Endpoints   []interface{} `json:"endpoints"`
+	Nodes       []Node       `json:"nodes"`
+	Connections []Connection `json:"connections"`
+}
+
+type Node struct {
+	Id            string                 `json:"id"`
+	Type          string                 `json:"type"`
+	Name          string                 `json:"name"`
+	Configuration map[string]interface{} `json:"configuration"`
+}
+
+type Connection struct {
+	FromId string `json:"fromId"`
+	ToId   string `json:"toId"`
+	Type   string `json:"type"`
+}
