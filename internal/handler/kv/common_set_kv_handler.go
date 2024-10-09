1ded0702dbf793088804417a32aa986883d3fbfd
xuetu.qiang <1227694865@qq.com>
2024-10-09 18:55:32 +0800

    feat update canvas logic

---
+++ internal/handler/kv/common_set_kv_handler.go
@@ -0,0 +1,25 @@
+package kv
+
+import (
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"net/http"
+	"workflow/internal/logic/kv"
+	"workflow/internal/svc"
+	"workflow/internal/types"
+	"workflow/response"
+)
+
+func CommonSetKvHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.SetKvRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := kv.NewCommonSetKvLogic(r.Context(), svcCtx)
+		resp, err := l.CommonSetKv(&req)
+		response.Response(w, resp, err)
+
+	}
+}
