93da2872868386ec4459b1501946854a299989a8
WYL <wuyuli995@163.com>
2024-12-19 10:23:14 +0800

    test oracle select sql

--- internal/rulego/mc_datasource_oracle_test.go
+++ internal/rulego/mc_datasource_oracle_test.go
@@ -30,9 +30,44 @@ func Test_roleChain_Run_Oracle_Delete(t *testing.T) {
 	)
 }
 
-func Test_roleChain_Run_Oracle_Inner_Join(t *testing.T) {
+func Test_roleChain_Run_Oracle_Select_Function(t *testing.T) {
 	setupAndRunChain(t,
-		"./chain/oracle/oracle_inner_join.json",
-		"{\"table\":\"users\",\"id\":11}",
+		"./chain/oracle/oracle_select_function.json",
+		"{\"table\":\"T_ACCOUNT\",\"id\":11}",
+	)
+}
+
+func Test_roleChain_Run_Oracle_Select_Inner_Join(t *testing.T) {
+	setupAndRunChain(t,
+		"./chain/oracle/oracle_select_inner_join.json",
+		"{\"table1\":\"T_OWNERS\",\"table2\":\"T_OWNERTYPE\",\"table3\":\"T_ADDRESS\",\"table4\":\"T_AREA\"}",
+	)
+}
+
+func Test_roleChain_Run_Oracle_Select_Left_Join(t *testing.T) {
+	setupAndRunChain(t,
+		"./chain/oracle/oracle_select_left_join.json",
+		"{\"table1\":\"T_OWNERS\",\"table2\":\"T_ACCOUNT\"}",
+	)
+}
+
+func Test_roleChain_Run_Oracle_Select_Right_Join(t *testing.T) {
+	setupAndRunChain(t,
+		"./chain/oracle/oracle_select_right_join.json",
+		"{\"table1\":\"T_OWNERS\",\"table2\":\"T_ACCOUNT\"}",
+	)
+}
+
+func Test_roleChain_Run_Oracle_Select_Sub_Select(t *testing.T) {
+	setupAndRunChain(t,
+		"./chain/oracle/oracle_select_sub_select.json",
+		"{\"table1\":\"T_OWNERS\",\"table2\":\"T_ACCOUNT\"}",
+	)
+}
+
+func Test_roleChain_Run_Oracle_Select_OrderBy_Limit(t *testing.T) {
+	setupAndRunChain(t,
+		"./chain/oracle/oracle_select_order_by_limit.json",
+		"{\"table1\":\"T_OWNERS\",\"table2\":\"T_ACCOUNT\"}",
 	)
 }
